6a37e829e2bb1d71da5500aaebdb9dc459768396
huyang8 <huyang8@jd.com>
2017-12-04 22:23:17 +0800

    前端提交

--- yoyo_weCat/pages/english/english.js
+++ yoyo_weCat/pages/english/english.js
@@ -1,46 +1,44 @@
+var app = getApp();
 Page({
   data: {
-    films: [],
-    hasMore: true,
-    showLoading: false,
-    start: 0,
-    englishCourse: [
-      {
-        "id" :"1",
-        "ecTitle" :"海员英语课程初级",
-        "ecImg": "https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3494076888,1278194760&fm=58",
-        "ecDesc": "海员英语课程初级海员英语课程初级海员英语课程初级海员英语课程初级海员英语课程初级海员英语课程初级"
-      },{
-        "id": "2",
-        "ecTitle": "海员英语课程中级",
-        "ecImg": "https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3494076888,1278194760&fm=58",
-        "ecDesc": "海员英语课程中级海员英语课程中级海员英语课程初级海员英语课程初级海员英语课程初级海员英语课程初级"
-      }, {
-        "id": "3",
-        "ecTitle": "海员英语课程高级",
-        "ecImg": "https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3494076888,1278194760&fm=58",
-        "ecDesc": "海员英语课程高级海员英语课程高级海员英语课程初级海员英语课程初级海员英语课程初级海员英语课程初级"
-      }, {
-        "id": "4",
-        "ecTitle": "海员英语课程特级",
-        "ecImg": "https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3494076888,1278194760&fm=58",
-        "ecDesc": "海员英语课程特级海员英语课程特级海员英语课程初级海员英语课程初级海员英语课程初级海员英语课程初级"
+    LIST: [],
+    payType: ["免费", "付费"],
+    imgUrl: app.globalData.hostUrl
+  },
+  onLoad: function (options) {
+    var that = this
+    this.title = options.title
+    wx.request({
+      url: app.globalData.hostUrl + '/admin.do?method=getAllEnglishClassData',
+      success: function (res) {
+        if (res.data.LIST <= 0) {
+          wx.showModal({
+            title: '提示',
+            content: '暂无相关英语课程！',
+            success: function () {
+              wx.navigateBack({
+                delta: 1
+              })
+            }
+          })
+        } else {
+          that.setData({
+            LIST: res.data.LIST
+          })
+        }
       }
-    ]
-  },
-  onPullDownRefresh: function () {
-    console.log('onPullDownRefresh', new Date())
-  },
-  scroll: function (e) {
-    //console.log(e)
-  },
-  scrolltolower: function () {
-    
+    })
   },
   viewDetail: function (e) {
     var ds = e.currentTarget.dataset;
     wx.navigateTo({
-      url: '../detail/detail?id=' + ds.id + '&title=' + ds.title + '&type=ing'
+      url: '../englishChap/englishChap?classId=' + ds.id
     })
-  }
+  },
+  scroll: function (e) {
+    
+  },
+  scrolltolower: function () {
+    
+  },
 })
