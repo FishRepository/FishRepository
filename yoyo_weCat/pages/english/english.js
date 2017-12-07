6316f9b74b0b821a4a1d315bd2ef6cf8eb20ac37
huyang8 <huyang8@jd.com>
2017-12-07 18:01:21 +0800

    微信小程序前端代码提交

--- yoyo_weCat/pages/english/english.js
+++ yoyo_weCat/pages/english/english.js
@@ -6,10 +6,14 @@ Page({
     imgUrl: app.globalData.hostUrl
   },
   onLoad: function (options) {
-    var that = this
-    this.title = options.title
+    var that = this;
+    this.title = options.title;
+    var openid = app.globalData.openid;
     wx.request({
       url: app.globalData.hostUrl + '/admin.do?method=getAllEnglishClassData',
+      data: {
+        "WID": openid
+      },
       success: function (res) {
         if (res.data.LIST <= 0) {
           wx.showModal({
@@ -31,9 +35,23 @@ Page({
   },
   viewDetail: function (e) {
     var ds = e.currentTarget.dataset;
-    wx.navigateTo({
-      url: '../englishChap/englishChap?classId=' + ds.id
-    })
+    var isPay = ds.ispay;
+    var payOver = ds.payover;
+    if (isPay==0){
+      wx.navigateTo({
+        url: '../englishChap/englishChap?classId=' + ds.id
+      })
+    }else{
+      if (payOver != undefined && payOver>=0){
+        wx.navigateTo({
+          url: '../englishChap/englishChap?classId=' + ds.id
+        })
+      }else{
+        wx.showToast({
+          title: '请先购买课程！',
+        })
+      }
+    }
   },
   scroll: function (e) {
     
