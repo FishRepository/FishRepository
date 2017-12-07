8255ef44a6b0aa6feb2de255cc9b1a29ef2c897d
huyang8 <huyang8@jd.com>
2017-12-07 17:30:22 +0800

    微信小程序前端代码提交

---
+++ yoyo_weCat/pages/englishPay/englishPay.js
@@ -0,0 +1,33 @@
+var app = getApp();
+Page({
+  data: {
+    LIST: []
+  },
+  onLoad: function (options) {
+    var that = this;
+    var openid = app.globalData.openid;
+    wx.request({
+      url: app.globalData.hostUrl + '/admin.do?method=getEnglishPayRecord',
+      data: {
+        "wid": openid
+      },
+      success: function (res) {
+        if (res.data.LIST <= 0) {
+          wx.showModal({
+            title: '提示',
+            content: '暂无英语课程购买记录！',
+            success: function () {
+              wx.navigateBack({
+                delta: 1
+              })
+            }
+          })
+        } else {
+          that.setData({
+            LIST: res.data.LIST
+          })
+        }
+      }
+    })
+  }
+})
