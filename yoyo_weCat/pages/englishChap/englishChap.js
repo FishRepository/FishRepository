488f4e9fb1792bf23acf94c7e8f6ccd54bdf8fae
huyang8 <huyang8@jd.com>
2017-12-18 14:08:48 +0800

    微信小程序前端代码提交

--- yoyo_weCat/pages/englishChap/englishChap.js
+++ yoyo_weCat/pages/englishChap/englishChap.js
@@ -4,7 +4,8 @@ Page({
   data: {
     LIST: [],
     choose: "",
-    sectionType: ""
+    sectionType: "",
+    value:[0]
   },
   bindChange: function (e) {
     var that = this;
@@ -12,50 +13,62 @@ Page({
     that.setData({
       choose: that.data.LIST[val].CHAP_NAME,
       sectionType: that.data.LIST[val].ID,
-      key: that.data.LIST[val].CLASS_ID + "-" + that.data.LIST[val].ID + "-" + that.data.LIST[val].CHAP_NAME + "key"
-    })
+      value: val
+    });
+    wx.setStorageSync(that.data.key, that.data);
   },
   onLoad: function (options) {
     var that = this;
     var classId = options.classId;
-    that.getChapByClassId(classId);
-  },
-  getChapByClassId: function (c) {
-    var that = this;
-    wx.request({
-      url: app.globalData.hostUrl + '/admin.do?method=getAllEnglishChapDataByClassId',
-      data: {
-        classId: c
-      },
+    that.setData({
+      key: classId+"-YYZJ-"+"key"
+    });
+    /*获取缓存*/
+    wx.getStorage({
+      key: that.data.key,
       success: function (res) {
-        if (res.data.LIST <= 0) {
-          wx.showModal({
-            title: '提示',
-            content: '该课程暂无章节目录！',
-            success: function () {
-              wx.navigateBack({
-                delta: 1
+        wx.hideLoading();
+        that.setData(res.data);
+      },
+      fail: function () {
+        wx.request({
+          url: app.globalData.hostUrl + '/admin.do?method=getAllEnglishChapDataByClassId',
+          data: {
+            classId: classId
+          },
+          success: function (res) {
+            if (res.data.LIST <= 0) {
+              wx.showModal({
+                title: '提示',
+                content: '该课程暂无章节目录！',
+                success: function () {
+                  wx.navigateBack({
+                    delta: 1
+                  })
+                }
+              })
+            } else {
+              that.setData({
+                LIST: res.data.LIST,
+                choose: res.data.LIST[0].CHAP_NAME,
+                sectionType: res.data.LIST[0].ID,
+                value: [0]
               })
             }
-          })
-        } else {
-          that.setData({
-            LIST: res.data.LIST,
-            choose: res.data.LIST[0].CHAP_NAME,
-            sectionType: res.data.LIST[0].ID,
-            key: res.data.LIST[0].CLASS_ID + "-" + res.data.LIST[0].ID + "-" + res.data.LIST[0].CHAP_NAME + "key"
-          })
-        }
+          }
+        })
       }
     })
   },
   clearSession: function () {
     var that = this;
+    var questionKey = that.data.sectionType+ "-ZJ-" + "key";
     wx.showModal({
       title: '提示',
       content: '清除此章节缓存后，再次进入将不会继续上一次章节测试',
       success: function (res) {
         if (res.confirm) {
+          wx.removeStorageSync(questionKey);
           wx.removeStorage({
             key: that.data.key,
             success: function (res) {
